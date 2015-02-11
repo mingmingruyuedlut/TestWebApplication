0274899608e6829d9bda993289a72851b7d63447
EricSun <mingmingruyuedlut@163.com>
2015-02-11 08:36:32 +0800

    test email

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -54,7 +54,7 @@ namespace ConsoleApplication2
             client.UseDefaultCredentials = false;
             client.Port = Int32.Parse(ConfigurationManager.AppSettings["MailPort"]);
 
-            NetworkCredential myCredentials = new NetworkCredential(ConfigurationManager.AppSettings["MailAccountName"], ConfigurationManager.AppSettings["FromAddressPwd"]);
+            NetworkCredential myCredentials = new NetworkCredential(ConfigurationManager.AppSettings["FromAddress"], ConfigurationManager.AppSettings["FromAddressPwd"]);
             client.Credentials = myCredentials;
 
             client.Send(message);
